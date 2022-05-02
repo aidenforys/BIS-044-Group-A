70324581bd44003914682fb857e2f02286063111
Aiden Forys <ajf225@lehigh.edu>
2022-05-02 14:24:37 -0400

    graphing box plots

--- Group A.R
+++ Group A.R
@@ -1,5 +1,14 @@
+install.packages("ggplot2")
 data <- read.csv("StudentsPerformance.csv")
 print(data)
-summary(data$math.score)
-summary(data$reading.score)
-summary(data$writing.score)
+data$test.preparation.course <- replace(data$test.preparation.course,data$test.preparation.course=="none",0)
+data$test.preparation.course <- replace(data$test.preparation.course,data$test.preparation.course=="completed",1)
+prepdata <- data[which(data$test.preparation.course>0.5),]
+noprepdata <- data[which(data$test.preparation.course<0.5),]
+print(noprepdata)
+summary(prepdata$math.score)
+summary(prepdata$reading.score)
+summary(prepdata$writing.score)
+summary(noprepdata$math.score)
+summary(noprepdata$reading.score)
+summary(noprepdata$writing.score)
